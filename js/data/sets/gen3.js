cbcf9d3c0e89f96f53e255f20babafcb4821aaaf
Connor McGowan <cpmcgowan28@gmail.com>
2023-02-12 15:20:06 -0500

    Added built files

---
+++ js/data/sets/gen3.js
@@ -0,0 +1,7 @@
+var SETDEX_ADV = {"Abra":{ "Psychic Edward": {"ability":"Synchronize","level":18,"moves":[ "Psychic", "Ice Punch", "Thunder Punch", "Fire Punch" ],"nature":"Timid"}}
+ ,"Absol":{"Ninja Boy Yasu": {"ability":"Pressure","level":27 ,"item":"Bright Powder","moves":[ "Bite", "Aerial Ace", "Will-O-Wisp", "Quick Attack"]}
+ ,"Ninja Boy Yasu": {"ability":"Pressure","level":45 ,"item":"Lum Berry","moves":[ "Shadow Ball", "Swords Dance", "Quick Attack", "Hidden Power Fighting" ],"nature":"Calm"}
+ ,"Ninja Boy Yasu": {"ability":"Pressure","level":52 ,"item":"Lum Berry","moves":[ "Aerial Ace", "Shadow Ball", "Body Slam", "Protect" ],"nature":"Bold"}
+ ,"Ninja Boy Yasu": {"ability":"Pressure","level":52 ,"item":"Spell Tag","moves":[ "Protect", "Substitute", "Shadow Ball", "Superpower" ],"nature":"Bold"}
+ ,"Ninja Boy Yasu": {"ability":"Pressure","level":57 ,"item":"Bright Powder","moves":[ "Thunder Wave", "Fire Blast", "Shadow Ball", "Superpower" ],"nature":"Lax"}
+,"Ninja Boy Yasu": {"ability":"Pressure","level":74 ,"item":"Lum Berry","moves":[ "Thunder Wave", "Shadow Ball", "Superpower", "Rock Slide" ],"nature":"Gentle"}}};
